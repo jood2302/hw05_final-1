f63f133ba43e9d3b1982a079fa34a87ba69aca8b
Ковылин Василий <coherentus@gmail.com>
2021-06-11 11:16:05 +0300

    some fix PEP8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.cache import cache_page
 
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, User
